312406f23bba15443d0a0207e28f0478011430b3
Omar Khalil Jaber <omar1623@edu.easj.dk>
2020-02-13 11:09:18 +0100

    Add project files.

---
+++ SharedCalculatorMain/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DLLSharedCalculator;
+
+namespace SharedCalculatorMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n");
+
+            Calculation calculator = new Calculation();
+
+            Console.WriteLine("Addition\n" + "Expected 4");
+            Console.WriteLine(calculator.Addition(2,2));
+
+            Console.WriteLine("\n" + "Substraction\n" + "Expected 1");
+            Console.WriteLine(calculator.Substraction(4,3));
+
+            Console.WriteLine("\n" + "Multiply\n" + "Expected 10");
+            Console.WriteLine(calculator.Multiply(5,2));
+
+            Console.WriteLine("\n" + "Divide\n" + "Expected 4");
+            Console.WriteLine(calculator.Divide(8,2));
+
+        }
+    }
+}
